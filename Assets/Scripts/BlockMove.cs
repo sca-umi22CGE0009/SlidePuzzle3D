c327e1e84499706cb4f0c4ddb132e29820b0aaba
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2024-01-13 12:30:43 +0900

    rayの設定途中

--- Assets/Scripts/BlockMove.cs
+++ Assets/Scripts/BlockMove.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using UnityEngine;
 
 public class BlockMove : MonoBehaviour
 {
     GameObject objAlpha;
-    GameObject clickObj;
+    [SerializeField,Header("Rayの長さ")] private float length = 2.5f;
 
-    bool isHit = false;
-    bool blockHit = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +18,72 @@ public class BlockMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = objAlpha.transform.position;
+        Vector3 start = new Vector3(pos.x, pos.y, pos.z);
+        //rayの方向
+        Vector3 aDir = new Vector3(-length, 0, 0);
+        Vector3 dDir = new Vector3(-length, 0, 0);
+        Vector3 wDir = new Vector3(0, length, 0);
+        Vector3 sDir = new Vector3(0, -length, 0);
+
+        Ray a = new Ray(start, aDir);
+        Ray d = new Ray(start, dDir);
+        Ray w = new Ray(start, wDir);
+        Ray s = new Ray(start, sDir);
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(mousePosition, out RaycastHit hitInfo))
             {
-                if (hitInfo.collider.CompareTag("Block")||hitInfo.collider.CompareTag("Alpha"))
+                if (Physics.Raycast(a, out RaycastHit ahitInfo))
                 {
-                    clickObj = hitInfo.collider.gameObject;
-                    //オブジェクト座標交換
-                    Vector3 pos = new Vector3(clickObj.transform.position.x, clickObj.transform.position.y, clickObj.transform.position.z);
-                    clickObj.transform.position = objAlpha.transform.position;
-                    objAlpha.transform.position = pos;
+                    if (hitInfo.collider.CompareTag("Block") && ahitInfo.collider.CompareTag("Block"))
+                    {
+                        GameObject aObj = ahitInfo.collider.gameObject;
+                        //オブジェクト座標交換
+                        Vector3 aPos = new Vector3(aObj.transform.position.x, aObj.transform.position.y, aObj.transform.position.z);
+                        aObj.transform.position = objAlpha.transform.position;
+                        objAlpha.transform.position = aPos;
+                    }
                 }
             }
+        }
+    }
+    void BlockJudge()
+    {
+
+    }
+    void RayJudge()
+    {
+
+
+        Vector3 pos = objAlpha.transform.position;
+        Vector3 start = new Vector3(pos.x, pos.y, pos.z);
+        //rayの方向
+        Vector3 aDir = new Vector3(length, 0, 0);
+        Vector3 dDir = new Vector3(-length, 0, 0);
+        Vector3 wDir = new Vector3(0, length, 0);
+        Vector3 sDir = new Vector3(0, -length, 0);
+
+        Ray a = new Ray(start, aDir);
+        Ray d = new Ray(start, dDir);
+        Ray w = new Ray(start, wDir);
+        Ray s = new Ray(start, sDir);
+
+        if (Physics.Raycast(d, out RaycastHit dhitInfo))
+        {
+            GameObject dObj = dhitInfo.collider.gameObject;
+            //オブジェクト座標交換
+            Vector3 dPos = new Vector3(dObj.transform.position.x, dObj.transform.position.y, dObj.transform.position.z);
+            dObj.transform.position = objAlpha.transform.position;
+            objAlpha.transform.position = dPos;
+        }
+        if (Physics.Raycast(w, out RaycastHit whitInfo))
+        {
+        }
+        if (Physics.Raycast(s, out RaycastHit shitInfo))
+        {
         }
     }
 }
