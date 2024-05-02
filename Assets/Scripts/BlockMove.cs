1335ae0775ddcfecfe84c50fcd0a1e99603fb7ce
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2024-05-02 17:04:02 +0900

    ブロック判定完了

--- Assets/Scripts/BlockMove.cs
+++ Assets/Scripts/BlockMove.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class BlockMove : MonoBehaviour
 {
     GameObject objAlpha;
+    GameObject objClick;
+
     //[SerializeField, Header("左")] private GameObject left;
     [SerializeField,Header("Rayの長さ")] private float length = 2.5f;
 
@@ -19,81 +23,52 @@ public class BlockMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = objAlpha.transform.position;
-        Vector3 start = new Vector3(pos.x, pos.y, pos.z);
-
-        Vector3 lDir = new Vector3(-length, 0, 0);
-        Vector3 rDir = new Vector3(length, 0, 0);
-
-        Ray rayLeft = new Ray(start, lDir);
-        Ray rayRight = new Ray(start, rDir);
-
         //マウスの判定
         if (Input.GetMouseButtonDown(0))
         {
             Ray mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(mousePosition, out RaycastHit hitInfo))
             {
-                        RayJudge();
+                if (hitInfo.collider.CompareTag("Block"))
+                {
+                    //選んだオブジェクト
+                    objClick = hitInfo.collider.gameObject;
+                    RayJudge();
+                }
             }
         }
-#if true
-        Debug.DrawRay(rayLeft.origin, rayLeft.direction * length, UnityEngine.Color.green);
-        Debug.DrawRay(rayRight.origin, rayRight.direction * length, UnityEngine.Color.red);
-#else
-        Debug.DrawRay(rayLeft.origin, rayLeft.direction * length, UnityEngine.Color.blue);
-        Debug.DrawRay(rayLeft.origin, rayLeft.direction * length, UnityEngine.Color.yellow);
-#endif
-    }
-    void BlockJudge()
-    {
-
     }
     void RayJudge()
     {
-        Vector3 pos = objAlpha.transform.position;
-        Vector3 start = new Vector3(pos.x, pos.y, pos.z);
         //rayの方向
         Vector3 lDir = new Vector3(length, 0, 0);
         Vector3 rDir = new Vector3(-length, 0, 0);
         Vector3 uDir = new Vector3(0, length, 0);
         Vector3 dDir = new Vector3(0, -length, 0);
 
-        Ray rayLeft = new Ray(pos, lDir);
-        Ray rayRight = new Ray(start, rDir);
-        //Ray rayUp = new Ray(start, uDir);
-        //Ray rayDown = new Ray(start, dDir);
-
-        if (Physics.Raycast(rayLeft, out RaycastHit hitL))
-        {
-            if (hitL.collider.CompareTag("Block"))
-            {
-                GameObject objL = hitL.collider.gameObject;
-                //オブジェクト座標交換
-                Vector3 PosL = new Vector3(objL.transform.position.x, objL.transform.position.y, objL.transform.position.z);
-                objL.transform.position = objAlpha.transform.position;
-                objAlpha.transform.position = PosL;
-            }
-        }
-
-        if (Physics.Raycast(rayRight, out RaycastHit hitR))
-        {
-            if (hitR.collider.CompareTag("Block"))
-            {
-                GameObject objR = hitR.collider.gameObject;
-                //オブジェクト座標交換
-                Vector3 posR = new Vector3(objR.transform.position.x, objR.transform.position.y, objR.transform.position.z);
-                objR.transform.position = objAlpha.transform.position;
-                objAlpha.transform.position = posR;
-            }
-        }
-        //if (Physics.Raycast(w, out RaycastHit whitInfo))
-        //{
-
-        //}
-        //if (Physics.Raycast(s, out RaycastHit shitInfo))
-        //{
-        //}
+        placeChanger(lDir);
+        placeChanger(rDir);
+        placeChanger(uDir);
+        placeChanger(dDir);
     }
 
+    void placeChanger(Vector3 dir)
+    {
+        Vector3 pos = objClick.transform.position;
+        Vector3 start = new Vector3(pos.x, pos.y, pos.z);
+
+        Ray ray = new Ray(pos, dir);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.collider.CompareTag("Alpha"))
+            {
+                GameObject obj = hit.collider.gameObject;
+                Vector3 tmp = obj.transform.position;
+                //オブジェクト座標交換
+                obj.transform.position = objClick.transform.position;
+                objClick.transform.position = tmp;
+            }
+        }
+    }
 }
